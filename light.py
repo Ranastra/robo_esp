4a999b092ca1c7da03a7d6ba21d72f3f926d52be
Ranastra <101139864+Ranastra@users.noreply.github.com>
2023-06-17 11:19:39 +0200

    working light sensors

--- light.py
+++ light.py
@@ -1,27 +1,29 @@
 import pinesp32
+from time import sleep_ms
 from machine import Pin, ADC
-from shift_register import shift_register_write, shift_register_set
+from shift_register import shift_register_write, shift_register_set, shift_register_reset
+shift_register_reset()
 
 # c++ https://github.com/waspinator/CD74HC4067
 
 _g_channel_truth_table = [
-    #s0, s1, s2, s3     channel
-    [1,  1,  1,  1],  # 0
-    [0,  1,  1,  1],  # 1
-    [1,  0,  1,  1],  # 2
-    [0,  0,  1,  1],  # 3
-    [1,  1,  0,  1],  # 4
-    [0,  1,  0,  1],  # 5
-    [1,  0,  0,  1],  # 6
-    [0,  0,  0,  1],  # 7
-    [1,  1,  1,  0],  # 8
-    [0,  1,  1,  0],  # 9
-    [1,  0,  1,  0],  # 10
-    [0,  0,  1,  0],  # 11
-    [1,  1,  0,  0],  # 12
-    [0,  1,  0,  0],  # 13
-    [1,  0,  0,  0],  # 14
-    [0,  0,  0,  0]   # 15
+    # s0, s1, s2, s3     channel
+    [0,  0,  0,  0],  # 0
+    [1,  0,  0,  0],  # 1
+    [0,  1,  0,  0],  # 2
+    [1,  1,  0,  0],  # 3
+    [0,  0,  1,  0],  # 4
+    [1,  0,  1,  0],  # 5
+    [0,  1,  1,  0],  # 6
+    [1,  1,  1,  0],  # 7
+    [0,  0,  0,  1],  # 8
+    [1,  0,  0,  1],  # 9
+    [0,  1,  0,  1],  # 10
+    [1,  1,  0,  1],  # 11
+    [0,  0,  1,  1],  # 12
+    [1,  0,  1,  1],  # 13
+    [0,  1,  1,  1],  # 14
+    [1,  1,  1,  1]   # 15
 ]
 
 _S0 = Pin(pinesp32.S0, Pin.OUT)
@@ -40,5 +42,9 @@ def read_raw(channel: int):
     set_channel(channel)
     return _ADC_MULTI.read_u16()
 
+print("te")
+print(read_raw(pinesp32.ADC_PT_M))
+print("te")
 while True:
+    sleep_ms(200)
     print(read_raw(pinesp32.ADC_PT_M))
